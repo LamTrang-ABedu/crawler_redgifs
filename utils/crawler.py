6a2c5fadbd63c30c4efb72effbf599b8db4cb2a2
Lam <LamTrang@abedu.vn>
2025-04-29 15:18:35 +0700

    a

--- utils/crawler.py
+++ utils/crawler.py
@@ -2,23 +2,24 @@ import requests
 from bs4 import BeautifulSoup
 
 # Hàm lấy Access Token miễn phí từ Redgifs API
-def get_access_token():
+def get_redgifs_token():
     try:
-        response = requests.post("https://api.redgifs.com/v2/oauth2/token", json={"grant_type": "client_credentials"})
-        response.raise_for_status()
-        return response.json()['access_token']
+        res = requests.get("https://api.redgifs.com/v2/auth/temporary", timeout=10)
+        if res.ok:
+            token = res.json().get('access_token')
+            return token
     except Exception as e:
         print(f"[Error] Get access token failed: {e}")
-        return None
+    return None
               
 def crawl(limit=30):
-    token = get_access_token()
+    token = get_redgifs_token()
     if not token:
         return []
     
     headers = {"Authorization": f"Bearer {token}"}
     try:
-        res = requests.get(f"https://api.redgifs.com/v2/gifs/search?search=popular&count={limit}", headers=headers)
+        res = requests.get(f"https://api.redgifs.com/v2/gifs/trending?count={limit}", headers=headers)
         res.raise_for_status()
         gifs = res.json().get('gifs', [])
         results = []
